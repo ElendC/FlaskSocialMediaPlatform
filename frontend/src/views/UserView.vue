05fd6b09d825a908c1679d2c15a5d42b6cba6008
Elend Egid <elende@hotmail.no>
2024-06-02 17:05:58 +0200

    userView started

---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <p v-if="username">Username: {{ username }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+    };
+  },
+  created() {
+    this.fetchCurrentUser();
+  },
+  methods: {
+    fetchCurrentUser() {
+      this.$http
+        .get("/current_user")
+        .then((response) => {
+          this.username = response.data.username;
+        })
+        .catch(() => {
+          console.error("Error fetching current user");
+        });
+    },
+  },
+};
+</script>
